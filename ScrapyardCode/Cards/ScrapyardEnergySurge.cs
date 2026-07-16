15a46648a9514862072bbd4a806984d3d17f67e8
Selinafire <242707694@qq.com>
2026-07-16 14:01:22 +0800

    Add files via upload

---
+++ ScrapyardCode/Cards/ScrapyardEnergySurge.cs
@@ -0,0 +1,44 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using Scrapyard.Characters;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace Scrapyard.Cards;
+
+[RegisterCard(typeof(ScrapyardCardPool))]
+public sealed class ScrapyardEnergySurge : ModCardTemplate
+{
+    private const int energyCost = 1;
+    private const CardType type = CardType.Skill;
+    private const CardRarity rarity = CardRarity.Uncommon;
+    private const TargetType targetType = TargetType.Self;
+    private const bool shouldShowInCardLibrary = true;
+
+    public override CardAssetProfile AssetProfile => new(
+        PortraitPath: $"{Entry.ResPath}/images/cards/{GetType().Name}.png");
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    public ScrapyardEnergySurge() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        int multiplier = base.IsUpgraded ? 12 : 6;
+        int currentEnergy = base.Owner.PlayerCombatState.Energy;
+        
+        // 翻倍：获得 (multiplier - 1) 倍当前能量
+        await PlayerCmd.GainEnergy(currentEnergy * (multiplier - 1), base.Owner);
+    }
+
+    protected override void OnUpgrade()
+    {
+        // 6倍 → 12倍
+    }
+}
